a9f13014f9e8b16c60ba6f250d7367de2426b20c
renu100 <renu.chandraker08@gmail.com>
2021-09-18 16:00:00 +0530

    Added: ensure winning position

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -3,17 +3,28 @@ package com.snakeladder;
 public class SnakeAndLadder {
 	public static void main(String[] args) {
 		int position = 0;
-		int dieRoll = 1 + (int) Math.floor(Math.random() * 10) % 6;
-		System.out.println("Die Rolled:" + dieRoll);
-		int checkOption = (int) Math.floor(Math.random() * 10) % 3;
-		if (checkOption == 0) {
-			System.out.println("No Play");
-		} else if (checkOption == 1) {
-			System.out.println("Ladder Comes");
-			position += dieRoll;
-		} else {
-			System.out.println("Snake Comes");
-			position -= dieRoll;
+		while (position != 100) {
+			int dieRoll = 1 + (int) Math.floor(Math.random() * 10) % 6;
+			System.out.println("Die Rolled:" + dieRoll);
+
+			int checkOption = (int) Math.floor(Math.random() * 10) % 3;
+			if (checkOption == 0) {
+				System.out.println("No Play");
+			} else if (checkOption == 1) {
+				System.out.println("Ladder Comes");
+				if (position + dieRoll <= 100) {
+					position += dieRoll;
+				} else if (position + dieRoll > 100) {
+					continue;
+				}
+			} else {
+				System.out.println("Snake Comes");
+				if ((position - dieRoll) < 0) {
+					position = 0;
+				} else {
+					position -= dieRoll;
+				}
+			}
 		}
 	}
 }
